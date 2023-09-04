6399f15172bad6cbc550163b95bd7307cb7460aa
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2023-09-04 12:04:00 +0800

    Update init.ts

--- init.ts
+++ init.ts
@@ -1,8 +1,8 @@
 // deno-lint-ignore-file
 import { mime } from "https://deno.land/x/mimetypes@v1.0.0/mod.ts";
 import { v4 as uuid4 } from 'https://esm.sh/uuid@9.0.0'
-import { db as DB } from "https://deno.land/x/jljiu@v1.0.01/mod.ts";
-import { ref, child, get, set } from "https://esm.sh/firebase@9.14.0/database"
+import { db as DB } from "https://deno.land/x/jljiu/mod.ts";
+import { ref, child, get, set } from "https://esm.sh/firebase/database"
 import { db } from "./updateDB.ts";
 let notionInfo = {
     id: 'a6315f71-8197-41af-8854-96a33bc0b155',
