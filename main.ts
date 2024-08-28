ae658a48d03f4c7d8a02a2213079d123e9bc69c0
jljiu9 <77003319+jljiu9@users.noreply.github.com>
2024-08-28 19:00:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,69 @@ const notionInfo = {
     cookie:'file_token=v02%3Afile_token%3A9kKJmDqMIIR34ZKjYldtgqVcMXI5y12ipipmmo_ZxCTSqm84M71QcAWiZe_3o5C-h4ulXh-V4dXTI3yUD9vgjjxAA0XoeMyXGNghTCTKF-Amf3h4V_kbRvB8B1qTOIavOifd8y5pyvU4akA4W0JmGJ7np4Fh; logglytrackingsession=1abcc622-b80e-425b-bd5e-77654841eb20; notion_experiment_device_id=be5d61a3-d85b-47bd-adca-b9c7880abe34; notion_browser_id=7919159a-b642-4ae9-a42c-3015b53e04c5; cb_user_id=null; cb_group_id=null; _ga=GA1.1.932389555.1664288357; _mkto_trk=id:414-XMY-838&token:_mch-www.notion.so-1664288356716-48733; intercom-device-id-gpfdrxfd=c78c58f2-e474-40f4-bff2-70fe6b46e674; tatari-cookie-test=25854168; intercom-id-gpfdrxfd=a21f227d-e325-4dc9-b2ab-6daaa178a506; cb_anonymous_id=%2208a09ba8-3e45-4890-ba05-ec7dcd617ec4%22; _cioid=db17c83bafcf46e4a5a8ce99f167c637; device_id=b3f16c18-a3b4-4ad8-8b57-3c94663788e9; _hjSessionUser_3664679=eyJpZCI6IjcwMGU0NDUxLTU4OWUtNTY4Yy04ODE0LWQwN2FkYmE2NTdlOSIsImNyZWF0ZWQiOjE3MDcyODIyMTI1MzIsImV4aXN0aW5nIjp0cnVlfQ==; tatari-session-cookie=0bddfb41-9cc9-b5aa-a403-0a8a857a4a6f; _uetvid=0eb54c3044f611ee8ce80b2316718ae4|g9zjm9|1707310268896|3|1|bat.bing.com/p/insights/c/o; _gcl_au=1.1.780540888.1724168009; token_v2=v02%3Auser_token_or_cookies%3AeOaVJPO-J5wsIdHp25CyqkrFCn1pG2aK1gVP-_Ens3rWrJzqQmmfdM2SIDO_2JUNPuFJqOR6HHNHHNc5nbZ1b6FWt4qrOYNroSTpn4jYjMsvSNjcRZ9_ccviLCR5sqAfcjOV; notion_user_id=db17c83b-afcf-46e4-a5a8-ce99f167c637; notion_cookie_consent={%22id%22:%2248222933-2b88-440d-a68f-2340c022bb48%22%2C%22permission%22:{%22necessary%22:true%2C%22targeting%22:true%2C%22preference%22:true%2C%22performance%22:true}%2C%22policy_version%22:%22v5%22}; notion_users=[%22db17c83b-afcf-46e4-a5a8-ce99f167c637%22]; NEXT_LOCALE=en-US; Metadata_visitor_id=m08l0djr11yn7x1k1swo; notion_check_cookie_consent=false; _ga_9ZJ8CB186L=GS1.1.1724601071.39.0.1724601071.60.0.0; _rdt_uuid=1724168009352.7569b69a-7a77-4dce-8505-061248d1b6a4; amp_af43d4=7919159ab6424ae9a42c3015b53e04c5.ZGIxN2M4M2JhZmNmNDZlNGE1YThjZTk5ZjE2N2M2Mzc=..1i651gkcj.1i65412cn.hdm.3tf.lb5; __cf_bm=fMGNnGzv7dzSIlQVJlRXHSTKPVzIcU3tajpgmeJ4XG0-1724683900-1.0.1.1-zLW16oX9v0LJEyQnZM27dOm3ECjYsfVEiH3EWvUvErCXz4MT2MRf1BiGFwo0Fk0fhUSF35isGdGSt5r8bQH3pw; _cfuvid=lxkkY_X877eZHE3mrlReU42RthCdq6fiCk24chpMaO8-1724683900429-0.0.1.1-604800000; notion_locale=en-US/user_choice'
 }
 
+// 刷新notion cookie
+const parseCookieString = (cookieString: string) =>{
+    const cookies = {};
+    cookieString.split(';').forEach(pair => {
+      const [name, value] = pair.trim().split('=');
+      cookies[name] = value;
+    });
+    return cookies;
+}
+async function refreshNotionSession(cookie: string) {
+    const response = await fetch("https://www.notion.so/f/refresh", {
+        "headers": {
+            "cookie": cookie,
+        },
+    });
+    const setCookieHeader = response.headers.get('set-cookie');
+    // 处理并更新cookie
+    let updatedCookies = {};
+    if (setCookieHeader) {
+        const cookieStrings = setCookieHeader.split(', ');
+        cookieStrings.forEach(cookieString => {
+            const [name, ...rest] = cookieString.split('=');
+            const value = rest.join('=').split(';')[0];
+            updatedCookies[name.trim()] = value;
+        });
+    }
+    // 合并新旧cookie
+    const oldCookies = parseCookieString(cookie);
+    const mergedCookies = { ...oldCookies, ...updatedCookies };
+    // 转换回字符串格式
+    const finalCookieString = Object.entries(mergedCookies)
+        .map(([name, value]) => `${name}=${value}`)
+        .join('; ');
+    return finalCookieString;
+}
+async function getNotionInfo() {
+    const notionInfoRef = ref(db, 'jsave/notioninfo');
+    const snapshot = await get(notionInfoRef);
+    if (snapshot.exists()) {
+        const data = snapshot.val();
+        const lastUpdated = new Date(data.lastUpdated);
+        const now = new Date();
+        if (now.getTime() - lastUpdated.getTime() > 3 * 60 * 60 * 1000) { // 3小时
+        // Cookie 过期，需要刷新
+        const newCookie = await refreshNotionSession(data.cookie);
+        data.cookie = newCookie;
+        data.lastUpdated = now.toISOString();
+        await set(notionInfoRef, data);
+        }
+        return data;
+    } else {
+        // 如果数据不存在，使用默认值并保存
+        const defaultNotionInfo = {
+            ...notionInfo,
+            lastUpdated: new Date().toISOString()
+        };
+        await set(notionInfoRef, defaultNotionInfo);
+        return defaultNotionInfo;
+    }
+}
+
+
+
 // 辅助函数
 const cl = (x: any): any => {
     console.log(x);
@@ -42,6 +105,7 @@ const setObjOfUrlSearchParams = (obj: { [x: string]: any }, url: URL) => {
 }
 
 const tempUrl = async (url: string, name: string): Promise<string> => {
+    let notionInfo = await getNotionInfo();
     const xx = new URL('https://www.notion.so/signed/' + encodeURIComponent(url));
     setObjOfUrlSearchParams({
         table: 'block',
